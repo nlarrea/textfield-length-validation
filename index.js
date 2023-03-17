c5eb92f2ccad83d6a628dae6aeda8603b99c4e19
nlarrea <larrea.naia@gmail.com>
2023-03-17 16:41:11 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+'use strict';
+
+/**
+ * Checks if the input text length is greater than the maximum admited.
+ * 
+ * @param {string} content textField.value
+ * @param {number} max 
+ * @returns {boolean}
+ */
+const ensureContentLength = (content, max) => {
+    return (content.length > max);
+}
+
+/**
+ * Returns a false only if the given text field length is greater than a maximum.
+ * 
+ * @listens onkeyup
+ * @param {Object} textField
+ * @param {Object} counter      span that contains the countdown of chars
+ * @param {number} maxCount     maximum admited chars
+ */
+exports.textFieldLengthValidation = function(textField, counter, maxCount){
+    textField.onkeyup = function(){
+        counter.innerHTML = maxCount - this.value.length;
+
+        if (ensureContentLength(this.value, maxCount)) {
+            alert(`The length of this input should be a maximum of ${maxCount} characters!`);
+            return false;
+        }
+    }
+}
